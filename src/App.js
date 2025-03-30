232ad070dfce562f3f5d6a9f5ebc465505902f2a
anuragvishwak <87541383+anuragvishwak@users.noreply.github.com>
2025-03-30 09:25:05 +0530

    implemented source and destination

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SearchBar from "./SearchBar";
 import { MdLocationPin } from "react-icons/md";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import MapController from "./MapController";
 
 const customIcon = new L.Icon({
   iconUrl: markerIcon,
@@ -16,47 +17,86 @@ const customIcon = new L.Icon({
   popupAnchor: [1, -34],
 });
 
-function MapController({ selectedLocation }) {
-  const map = useMap();
-  if (selectedLocation) {
-    map.flyTo([selectedLocation.lat, selectedLocation.lon], 13, {
-      animate: true,
-      duration: 2,
-    });
-  }
-  return null;
-}
-
 function App() {
   const [openingSearchBar, setOpeningSearchBar] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  // const [suggestionBox, setsuggestionBox] = useState(false);
+  const [query, setQuery] = useState("");
   const [selectedLocation, setSelectedLocation] = useState({
     name: "Mumbai, India",
     lat: 19.076,
     lon: 72.8777,
   });
 
+  const handleSelect = (place) => {
+    setSelectedLocation({
+      name: place.display_name,
+      lat: parseFloat(place.lat),
+      lon: parseFloat(place.lon),
+    });
+    localStorage.setItem("recent location", "something to written here");
+    // setOpeningSearchBar(false);
+  };
+
+  const fetchLocations = async (input) => {
+    if (!input) return setSuggestions([]);
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${input}`
+    );
+    const data = await response.json();
+    setSuggestions(data);
+  };
+
   return (
     <div className="">
-      <div className="flex absolute left-16 top-5 z-10 items-center space-x-2">
+      <div className="flex absolute sm:w-5/12 bg-white py-2 px-2 rounded-full shadow-2xl sm:left-10 sm:top-5 m-4 sm:m-0 z-10 items-center space-x-2">
         <input
-          onClick={() => setOpeningSearchBar(true)}
+          onChange={(e) => {
+            setQuery(e.target.value);
+            fetchLocations(e.target.value);
+          }}
           placeholder="Search locations..."
-          className="py-1.5 px-4 w-60 sm:w-80 rounded-full border border-gray-300"
+          className="py-1.5 px-4 w-60 sm:w-full rounded-full border border-gray-300"
         />
-        <button className="bg-[#333333] text-white p-2 rounded-full">
-          <FaSearchLocation size={20} />
+        <button
+          onClick={() => setOpeningSearchBar(true)}
+          className="text-white bg-[#333333] rounded-full font-semibold py-1.5 px-4"
+        >
+          Travel
         </button>
       </div>
-     <div className="flex justify-end">
-     <button className="absolute m-7 bg-white p-2 z-10 rounded-full bottom-0">
-        <MdLocationPin size={30} />
-      </button>
-     </div>
+      <div className="flex justify-end">
+        <button className="absolute m-7  bg-white p-2 z-10 rounded-full bottom-0">
+          <MdLocationPin size={30} />
+        </button>
+      </div>
+
+      <div
+        className={`absolute top-16 sm:top-20 left-4 sm:left-10 w-[335px] sm:w-[640px] z-10 ${
+          openingSearchBar === true ? "hidden" : "block"
+        }`}
+      >
+        {suggestions.length > 0 && (
+          <ul className="mt-5 border bg-white p-5 h-96 overflow-auto rounded-2xl shadow-lg">
+            {suggestions.map((place) => (
+              <li
+                key={place.place_id}
+                onClick={() => handleSelect(place)}
+                className="py-1 cursor-pointer hover:bg-gray-200"
+              >
+                {place.display_name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
       <MapContainer
+        setSuggestions={setSuggestions}
         center={[selectedLocation.lat, selectedLocation.lon]}
         zoom={13}
-        className="h-screen w-full z-0 rounded-lg border"
+        zoomControl={false}
+        className="h-screen z-0 w-full"
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -69,16 +109,22 @@ function App() {
           <Popup>{selectedLocation.name}</Popup>
         </Marker>
 
-        <MapController selectedLocation={selectedLocation} />
+        <MapController
+          selectedLocation={selectedLocation}
+          setSuggestions={setSuggestions}
+        />
       </MapContainer>
-
-      
 
       {openingSearchBar && (
         <SearchBar
           selectedLocation={selectedLocation}
           setSelectedLocation={setSelectedLocation}
           setopeningSearchBar={setOpeningSearchBar}
+          setSuggestions={setSuggestions}
+          suggestions={suggestions}
+          query={query}
+          setQuery={setQuery}
+          fetchLocations={fetchLocations}
         />
       )}
     </div>
