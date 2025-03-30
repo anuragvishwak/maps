e4121c60c685e6dd0443137020fd6c8175e0dc70
anuragvishwak <87541383+anuragvishwak@users.noreply.github.com>
2025-03-30 22:19:02 +0530

    impletemented the source and destination  functionality

--- src/MapController.js
+++ src/MapController.js
@@ -1,19 +1,24 @@
 import { useEffect } from "react";
 import { useMapEvent } from "react-leaflet";
 
-function MapController({ selectedLocation, setSuggestions }) {
+function MapController({ selectedLocation, sourceCoords, destinationCoords, setSuggestions }) {
   const map = useMapEvent("moveend", () => {
     setSuggestions([]); 
   });
 
   useEffect(() => {
-    if (selectedLocation) {
-      map.flyTo([selectedLocation.lat, selectedLocation.lon], 13, {
-        animate: true,
-        duration: 2,
-      });
+    if (sourceCoords && destinationCoords) {
+      // Fit both source and destination inside the map with animation
+      const bounds = [
+        [sourceCoords.lat, sourceCoords.lon],
+        [destinationCoords.lat, destinationCoords.lon],
+      ];
+      map.flyToBounds(bounds, { padding: [50, 50], duration: 2 });
+    } else if (selectedLocation) {
+      // Fly to selected location
+      map.flyTo([selectedLocation.lat, selectedLocation.lon], 13, { animate: true, duration: 2 });
     }
-  }, [selectedLocation, map]);
+  }, [selectedLocation, sourceCoords, destinationCoords, map]);
 
   return null;
 }
